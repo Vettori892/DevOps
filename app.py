9df68d535736a62a442228b2b4eca219e23caab9
Vettori892 <vettori892@gmail.com>
2026-08-14 20:50:47 -0300

    Update app.py
    Adicionado entrada de nome e da idade do usuário.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-print("Projeto DevOps iniciado!")
 nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
 print("Olá,", nome)
+print("Você tem", idade, "anos.")
 print("Projeto DevOps em Python!")
