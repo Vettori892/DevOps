ab98cd84af258e830a80d6dd70a3663a7f832abe
Vettori892 <vettori892@gmail.com>
2026-08-14 20:52:38 -0300

    Refactor greeting to use saudacao function
    Integra app com módulo de mensagens

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from mensagens import saudacao
+
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
-print("Olá,", nome)
+print(saudacao(nome))
 print("Você tem", idade, "anos.")
 print("Projeto DevOps em Python!")
